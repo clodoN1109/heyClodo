cd4ddcfb701152ffa9bfceb5a1a3f6445b8e5929
clodo <104923248+clodoN1109@users.noreply.github.com>
2022-08-13 18:39:57 -0300

    main-page-launch

---
+++ assets/images/Who.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+class Node {
+  constructor (positionX,positionY,positionZ,time) {
+
+    this.value1 = Um observador fascinado com a complexidade até das 
+    coisas mais simples;
+
+   	this.value2 = Um pesquisador apaixonado por toda forma de desafio;
+
+   	this.value3 = Um programador implementando a ideia de um mundo 
+   	conectado de forma mais profunda e cooperativa.
+
+
+	traverseToIndexAndMakeItBetter(index, better) {
+    
+    	var count;
+    	var aux;
+    
+    	count = 1;
+    	aux = this.head;
+
+   	}
+
+
+  }
+}
+
+ const Clodo = new Node(positionX,positionY,positionZ,time);
+ traverseToIndexAndMakeItBetter(index, better); 
+
+
+
